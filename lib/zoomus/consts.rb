76268d091a0d39632ca8942cfe4f45395500466d
Narath Carlile <narathc@gmail.com>
2015-04-26 16:01:15 -0400

    Added basic constants

---
+++ lib/zoomus/consts.rb
@@ -0,0 +1,19 @@
+module Zoomus
+
+  LOGIN_TYPE = {
+      api_user: 99
+  }
+
+  USER_TYPE = {
+      basic: 1,
+      pro: 2,
+      corporate: 3
+  }
+
+  MEETING_TYPE = {
+      instant: 1,
+      scheduled: 2,
+      recurring: 3
+  }
+
+end
